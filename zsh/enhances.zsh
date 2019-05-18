12ebf827e14ef86abf421fdccd0bfd3c2f7aa4c1
hushicai <bluthcy@gmail.com>
2019-05-18 19:26:09 +0800

    update zsh

--- zsh/enhances.zsh
+++ zsh/enhances.zsh
@@ -1,7 +1,7 @@
 # https://github.com/junegunn/fzf/issues/467
-fzf-history-widget-accept() {
-  fzf-history-widget
-  zle accept-line
-}
-zle     -N   fzf-history-widget-accept
-bindkey '^R' fzf-history-widget-accept
+# fzf-history-widget-accept() {
+  # fzf-history-widget
+  # zle accept-line
+# }
+# zle     -N   fzf-history-widget-accept
+# bindkey '^R' fzf-history-widget-accept
