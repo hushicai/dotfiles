2bf209e34b28b6242ec881bfdfa63d13cff3b9ef
hushicai <bluthcy@gmail.com>
2019-06-02 21:06:05 +0800

    fzf use rg

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -10,3 +10,5 @@ if which ruby >/dev/null && which gem >/dev/null; then
     PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$(ruby -r rubygems -e 'puts Gem.dir')/bin:$PATH"
 fi
 
+# fzf
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow'
