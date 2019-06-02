7ef7313124033d34790d35557df9f2bea0e1b624
hushicai <bluthcy@gmail.com>
2019-06-02 21:07:40 +0800

    add zsh options
    prevent duplicates

---
+++ zsh/options.zsh
@@ -0,0 +1,9 @@
+# Remove superfluous blanks from each command line being added to the history
+# list
+setopt histreduceblanks
+# Remove command lines from the history list when the first character on the
+# line is a space, or when one of the expanded aliases contains a leading space
+setopt histignorespace
+# Do not enter command lines into the history list if they are duplicates of the
+# previous event.
+setopt histignorealldups
